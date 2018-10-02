691f63d5dea9e0c3b09c0bafd231d68ae1a394ca
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2018-10-02 12:03:57 +0200

    Rescale coefficients to MHz frequencies using math or polyfit

--- scripts/astrokat-fitflux.py
+++ scripts/astrokat-fitflux.py
@@ -1,24 +1,9 @@
 # Scaling flux calibrator coefficient to MHz
 # Reference: Perley & Butler 2017
 
+import argparse
 import numpy
 import matplotlib.pyplot as plt
-
-
-# Find index and nearest value in array
-def find_nearest(array, value):
-    array = numpy.asarray(array)
-    idx = (numpy.abs(array - value)).argmin()
-    return idx, array[idx]
-
-
-# Calculating flux density from polynomial equation
-def calc_flux_density(coeffs, freqs):
-    log10S = [coeffs[0]]*len(freqs)
-    for idx, coeff in enumerate(coeffs[1:]):
-        log10S += coeff * (numpy.log10(freqs)**(idx+1))
-    return log10S
-
 
 # Listed frequencies used for fitting
 fit_frequencies_mhz = numpy.array([
@@ -40,12 +25,28 @@ flux_cals = {  # name : Flux model (Min freq [MHz] Max freq [MHz] [model coeffic
         }
 
 
+# Find index and nearest value in array
+def find_nearest(array, value):
+    array = numpy.asarray(array)
+    idx = (numpy.abs(array - value)).argmin()
+    return idx, array[idx]
 
-for calib in flux_cals.keys():
-    # Flux density of calibrator over frequency using fitted coefficients
-    min_freq_mhz = flux_cals[calib][0]
-    max_freq_mhz = flux_cals[calib][1]
-    coeffs = flux_cals[calib][2:]
+
+# Calculating flux density from polynomial equation
+def calc_flux_density(coeffs, freqs):
+    log10S = [coeffs[0]]*len(freqs)
+    for idx, coeff in enumerate(coeffs[1:]):
+        log10S += coeff * (numpy.log10(freqs)**(idx+1))
+    return log10S
+
+
+# Scale the polynomial coefficients using a numpy refit
+def coeffs_ghz2mhz(
+        coeffs,
+        min_freq_mhz=50,
+        max_freq_mhz=50000,
+        verbose=False,
+        ):
 
     [min_idx, value] = find_nearest(fit_frequencies_mhz, min_freq_mhz)
     [max_idx, value] = find_nearest(fit_frequencies_mhz, max_freq_mhz)
@@ -61,21 +62,73 @@ for calib in flux_cals.keys():
     coeffs_ = numpy.polyfit(x, y, deg)[::-1]
     log10S_ = calc_flux_density(coeffs_, 10.**x)
 
-    fig = plt.figure(figsize=(8,11), facecolor='white')
-    plt.subplot(211)
-    plt.loglog(fit_freq_ghz, 10.**log10S)
-    plt.title(calib)
-    plt.ylabel('Flux Density [Jy]')
-    plt.xlabel('Frequency(GHz)')
-    plt.axis('tight')
-    plt.subplot(212)
-    plt.loglog(10.**x, 10.**y, 'b', 10.**x, 10.**log10S_, 'r--')
-    plt.ylabel('Flux Density [Jy]')
-    plt.xlabel('Frequency(MHz)')
-    plt.axis('tight')
+    if verbose:
+        fig = plt.figure(figsize=(8,11), facecolor='white')
+        plt.subplot(211)
+        plt.loglog(fit_freq_ghz, 10.**log10S)
+        plt.title(calib)
+        plt.ylabel('Flux Density [Jy]')
+        plt.xlabel('Frequency(GHz)')
+        plt.axis('tight')
+        plt.subplot(212)
+        plt.loglog(10.**x, 10.**y, 'b', 10.**x, 10.**log10S_, 'r--')
+        plt.ylabel('Flux Density [Jy]')
+        plt.xlabel('Frequency(MHz)')
+        plt.axis('tight')
 
-    print calib, coeffs_
+    return coeffs_
 
-plt.show()
+## scaling the flux model coefficients from the GHz used to MHz
+# scale coefficients using simple math: python astrokat-fitflux.py
+# scale coefficients by refitting the polynomial: python astrokat-fitflux.py --refit
+if __name__ == '__main__':
+    version = "%%prog 0.1"
+    usage = "%%prog [options]"
+    description = 'Scale flux model coefficients from GHz to MHz for MeerKAT telescope'
 
+    parser = argparse.ArgumentParser(
+            usage=usage,
+            description=description)
+    parser.add_argument(
+            '--version',
+            action='version',
+            version=version)
+    parser.add_argument(
+            '--refit',
+            action='store_true',
+            help='Redo polynomial fit using MHz frequencies')
+    parser.add_argument(
+            '--verbose',
+            action='store_true',
+            help='Display flux model results with refitting coefficients.')
+    args = parser.parse_args()
 
+    for calib in flux_cals.keys():
+        # Flux density of calibrator over frequency using fitted coefficients
+        min_freq_mhz = flux_cals[calib][0]
+        max_freq_mhz = flux_cals[calib][1]
+        coeffs = flux_cals[calib][2:]
+        if args.refit:
+            coeffs_ = coeffs_ghz2mhz(coeffs,
+                    min_freq_mhz=min_freq_mhz,
+                    max_freq_mhz=max_freq_mhz,
+                    verbose=args.verbose)
+        else:
+            [A, B] = coeffs[:2]
+            C = 0
+            if len(coeffs) > 2:
+                C = coeffs[2]
+            D = 0
+            if len(coeffs) > 3:
+                D = coeffs[3]
+            a = A - 3*B + 9*C - 27*D
+            b = B - 6*C + 27*D
+            c = C - 9*D
+            d = D
+            coeffs_ = numpy.array([a, b, c, d])
+        print calib, coeffs_
+
+    if args.verbose and args.refit:
+        plt.show()
+
+# -fin-
