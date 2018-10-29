438f942279037776cea18e149c7b9341fa50fadc
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2018-10-29 15:10:11 +0200

    Added test observation scripts & yaml files for various observation and setup types

---
+++ tests/check-offline-observe.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW='\033[0;33m'
+NOCOLOR="\033[0m"
+
+INPUT=(
+targets-sim
+two-calib-sim
+nd-pattern-sim
+nd-pattern-plus-off
+nd-pattern-cycle
+nd-pattern-ants
+drift-targets-sim
+raster-scans-sim
+image-single-sim
+image-sim)
+
+for infile in ${INPUT[@]}
+do
+    echo
+    CMD="python astrokat-observe.py --yaml test_input/$infile.yaml"
+    echo -e "${YELLOW} Testing: $CMD ${NOCOLOR}"
+    if $CMD
+    then
+        echo -e "${GREEN} Success ${NOCOLOR}"
+    else
+        echo -e "${RED} Failure ${NOCOLOR}"
+        break
+    fi
+done
+
+
+# -fin-
+
