fd68a18cbe82d38815f02523ea770f4d7ca86b88
Unknown <wbode@ska.ac.za>
2018-11-16 11:13:49 +0200

    Clean up file
    JIRA: MT-272

--- astrokat/test/test_offline_observe.py
+++ astrokat/test/test_offline_observe.py
@@ -1,12 +1,21 @@
-
+###############################################################################
+# SKA South Africa (http://ska.ac.za/)                                        #
+# Author: cam@ska.ac.za                                                       #
+# Copyright @ 2013 SKA SA. All rights reserved.                               #
+#                                                                             #
+# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
+# WRITTEN PERMISSION OF SKA SA.                                               #
+###############################################################################
 from __future__ import print_function
 from testconfig import config
 import unittest2 as unittest
 
 from astrokat import observe_main
 
-class test_astrokat(unittest.TestCase):
-    def print_all_tests(self):
-        import sys
+
+class test_astrokat_yaml(unittest.TestCase):
+    def run_all_tests(self):
         for test in config['yaml_test_list']:
-            status = observe_main.main(['--observer', 'jenkins','--yaml', 'astrokat/test/test_obs/{}.yaml'.format(test)])
+            print('Running test: {}'.format(test))
+            observe_main.main(['--observer', 'jenkins', '--yaml',
+                               'astrokat/test/test_obs/{}.yaml'.format(test)])
