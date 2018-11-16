6888dd206ce1c8a518a7f28414cca298325184f6
Unknown <wbode@ska.ac.za>
2018-11-16 13:31:42 +0200

    Remove license banner
    JIRA: MT-272

--- astrokat/test/test_offline_observe.py
+++ astrokat/test/test_offline_observe.py
@@ -1,11 +1,3 @@
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
 from __future__ import print_function
 from testconfig import config
 import unittest2 as unittest
