9d2a1d67c9b8a84aa9a8bc68a1a20f5b231d71c3
Unknown <wbode@ska.ac.za>
2018-11-16 13:30:56 +0200

    Remove license banner
    JIRA: MT-272

--- scripts/astrokat-observe.py
+++ scripts/astrokat-observe.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
-###############################################################################
-# SKA South Africa (http://ska.ac.za/)                                        #
-# Author: cam@ska.ac.za                                                       #
-# Copyright @ 2013 SKA SA. All rights reserved.                               #
-#                                                                             #
-# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
-# WRITTEN PERMISSION OF SKA SA.                                               #
-###############################################################################
 import sys
 
 from astrokat.observe_main import main
 
 
 if __name__ == '__main__':
-    import sys
     sys.exit(main(sys.argv[1:]))
