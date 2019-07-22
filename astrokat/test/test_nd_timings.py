6768e9c582264277070af388df4019edeb81a7fa
Mpho Mphego <mpho112@gmail.com>
2019-07-22 15:05:30 +0200

    Patching `Telescope` class and tests refactor
    This commit,
    - Adds license
    - Using mock patch to override the `Telescope` class
    - Removed redundant code, Refactored code and created `astrokat_testutils` which houses utility functions and
    classes
    
    JIRA: [MT-828](https://skaafrica.atlassian.net/browse/MT-828)
    
    Signed-off-by: Mpho Mphego <mmphego@ska.ac.za>

--- astrokat/test/test_nd_timings.py
+++ astrokat/test/test_nd_timings.py
@@ -1,17 +1,20 @@
+###############################################################################
+# SKA South Africa (http://ska.ac.za/)                                        #
+# Author: cam@ska.ac.za                                                       #
+# Copyright @ 2019 SKA SA. All rights reserved.                               #
+#                                                                             #
+# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
+# WRITTEN PERMISSION OF SKA SA.                                               #
+###############################################################################
+
 from __future__ import print_function
 
-import logging
 import os
-import sys
-
 import unittest2 as unittest
-from astrokat import observe_main, simulate
+from mock import patch
 
-if sys.version_info[0] == 3:
-    from io import StringIO
-else:
-    from StringIO import StringIO
-
+from astrokat import observe_main
+from astrokat_testutils import yaml_path, LoggedTelescope
 
 # - nd-pattern-sim
 # - nd-pattern-ants
@@ -22,46 +25,25 @@ else:
 PROPOSAL_ID = "CAM_AstroKAT_UnitTest"
 OBSERVER = "KAT Tester"
 
-TESTS_PATH = os.path.abspath(os.path.dirname(__file__))
 
-
+@patch('astrokat.observe_main.Telescope', LoggedTelescope)
 class test_astrokat_yaml(unittest.TestCase):
-    def setUp(self):
-        user_logger = logging.getLogger("astrokat.simulate")
-
-        # remove current handlers
-        for handler in user_logger.handlers:
-            user_logger.removeHandler(handler)
-
-        self.string_stream = StringIO()
-        out_hdlr = logging.StreamHandler(self.string_stream)
-        formatter = logging.Formatter("%(asctime)s - %(message)s")
-        formatter.formatTime = simulate.sim_time
-        out_hdlr.setFormatter(formatter)
-        out_hdlr.setLevel(logging.TRACE)
-        user_logger.addHandler(out_hdlr)
-        user_logger.setLevel(logging.INFO)
-
-    def yaml_path(self, file_path):
-        yaml_file = os.path.abspath(os.path.join(TESTS_PATH, file_path))
-        self.assertTrue(os.path.isfile(yaml_file))
-        return yaml_file
 
     def test_nd_pattern_sim(self):
-        yaml_file = self.yaml_path('test_nd/nd-pattern-sim.yaml')
+        yaml_file = yaml_path('test_nd/nd-pattern-sim.yaml')
         observe_main.main([
             "--yaml", yaml_file,
             "--observer", OBSERVER,
             "--proposal-id", PROPOSAL_ID,
             "--start-time", os.getenv("START_TIME", "2019-07-15 23:35:00"),
-            "--sb-id-code", os.getenv("SB_ID_CODE", "20190718-0001"),
+            "--sb-id-code", os.getenv("SB_ID_CODE"),
             "--dry-run",
         ])
 
         # TODO: restore this check after working out an appropriate start-time
         # in UTC with Ruby
 
-        result = self.string_stream.getvalue()
+        result = LoggedTelescope.string_stream.getvalue()
         # self.assertIn('Single run through observation target list', result, 'Single run')
         # self.assertIn('azel observed for 300.0 sec',
         #               result, 'azel observed for 300.0 sec')
--- astrokat/test/test_offline_observe.py
+++ astrokat/test/test_offline_observe.py
@@ -1,45 +1,30 @@
+###############################################################################
+# SKA South Africa (http://ska.ac.za/)                                        #
+# Author: cam@ska.ac.za                                                       #
+# Copyright @ 2019 SKA SA. All rights reserved.                               #
+#                                                                             #
+# THIS SOFTWARE MAY NOT BE COPIED OR DISTRIBUTED IN ANY FORM WITHOUT THE      #
+# WRITTEN PERMISSION OF SKA SA.                                               #
+###############################################################################
+
 from __future__ import print_function
 
-import logging
 import os
-import sys
 import unittest2 as unittest
+from mock import patch
 
-from six.moves import StringIO
-from astrokat import observe_main, simulate
-
+from astrokat import observe_main
+from astrokat_testutils import yaml_path, LoggedTelescope
 
 PROPOSAL_ID = "CAM_AstroKAT_UnitTest"
 OBSERVER = "KAT Tester"
-TESTS_PATH = os.path.abspath(os.path.dirname(__file__))
 
 
+@patch('astrokat.observe_main.Telescope', LoggedTelescope)
 class TestAstrokatYAML(unittest.TestCase):
 
-    def setUp(self):
-        user_logger = observe_main.user_logger
-
-        # remove current handlers
-        for handler in user_logger.handlers:
-            user_logger.removeHandler(handler)
-
-        self.string_stream = StringIO()
-        out_hdlr = logging.StreamHandler(self.string_stream)
-        formatter = logging.Formatter('%(asctime)s - %(message)s')
-        formatter.formatTime = simulate.sim_time
-        out_hdlr.setFormatter(formatter)
-        out_hdlr.setLevel(logging.TRACE)
-        user_logger.addHandler(out_hdlr)
-        user_logger.setLevel(logging.INFO)
-
-    def yaml_path(self, file_path):
-        """Convenience method for finding the yaml's file absolute path."""
-        yaml_file = os.path.abspath(os.path.join(TESTS_PATH, file_path))
-        self.assertTrue(os.path.isfile(yaml_file))
-        return yaml_file
-
     def test_targets_sim(self):
-        yaml_file = self.yaml_path('test_obs/targets-sim.yaml')
+        yaml_file = yaml_path('test_obs/targets-sim.yaml')
         observe_main.main([
             '--yaml', yaml_file,
             '--observer', OBSERVER,
@@ -47,14 +32,11 @@ class TestAstrokatYAML(unittest.TestCase):
             '--start-time', os.getenv('START_TIME', '2019-07-15 23:35:00'),
             '--sb-id-code', os.getenv('SB_ID_CODE'),
             '--dry-run',
-           ])
-
+        ])
         # get result and make sure everything ran properly
-        result = self.string_stream.getvalue()
-
+        result = LoggedTelescope.string_stream.getvalue()
         # TODO: restore this check after working out an appropriate start-time
         # in UTC with Ruby
-
         self.assertIn('Single run through observation target list', result, 'Single run')
         # self.assertIn('target0_radec observed for 10.0 sec',
         #               result, 'target0_radec observed for 10.0 sec')
@@ -64,7 +46,7 @@ class TestAstrokatYAML(unittest.TestCase):
         #               result, 'target2_gal observed for 10.0 sec')
 
     def test_two_calib_sim(self):
-        yaml_file = self.yaml_path('test_obs/two-calib-sim.yaml')
+        yaml_file = yaml_path('test_obs/two-calib-sim.yaml')
         observe_main.main([
             '--yaml', yaml_file,
             '--observer', OBSERVER,
@@ -72,11 +54,9 @@ class TestAstrokatYAML(unittest.TestCase):
             '--start-time', os.getenv('START_TIME', '2019-07-15 23:35:00'),
             '--sb-id-code', os.getenv('SB_ID_CODE'),
             '--dry-run',
-       ])
-
-
+        ])
         # get result and make sure everything ran properly
-        result = self.string_stream.getvalue()
+        result = LoggedTelescope.string_stream.getvalue()
         self.assertIn('Single run through observation target list', result, 'Single run')
         self.assertIn('Bandpass calibrators are [\'1934-638\', \'0408-65\']', result,
                       'two Bandpass calibrators')
@@ -84,7 +64,7 @@ class TestAstrokatYAML(unittest.TestCase):
         self.assertIn('0408-65 observed', result)
 
     def test_drift_targets_sim(self):
-        yaml_file = self.yaml_path('test_obs/drift-targets-sim.yaml')
+        yaml_file = yaml_path('test_obs/drift-targets-sim.yaml')
         observe_main.main([
             '--yaml', yaml_file,
             '--observer', OBSERVER,
@@ -92,11 +72,9 @@ class TestAstrokatYAML(unittest.TestCase):
             '--start-time', os.getenv('START_TIME', '2019-07-15 23:35:00'),
             '--sb-id-code', os.getenv('SB_ID_CODE'),
             '--dry-run',
-       ])
-
-
+        ])
         # get result and make sure everything ran properly
-        result = self.string_stream.getvalue()
+        result = LoggedTelescope.string_stream.getvalue()
         self.assertIn('Single run through observation target list', result, 'Single run')
         self.assertIn('0408-65 observed', result)
         self.assertIn('1934-638 observed', result)
@@ -109,14 +87,14 @@ class TestAstrokatYAML(unittest.TestCase):
     #                        'astrokat/test/test_obs/{}.yaml'.format('raster-scans-sim')])
     #
     #     # get result and make sure everything ran properly
-    #     result = self.string_stream.getvalue()
+    #     result = LoggedTelescope.string_stream.getvalue()
     #     self.assertIn('Single run through observation target list', result, 'Single run')
     #     self.assertIn('0408-65 observed', result, '0408-65 observed')
     #     self.assertIn('1934-638 observed', result, '1934-638 observed')
     #     self.assertEqual(result.count('Drift_scan observation for'), 2, 'two drift scans')
 
     def test_image_single_sim(self):
-        yaml_file = self.yaml_path('test_obs/image-single-sim.yaml')
+        yaml_file = yaml_path('test_obs/image-single-sim.yaml')
         observe_main.main([
             '--yaml', yaml_file,
             '--observer', OBSERVER,
@@ -126,23 +104,19 @@ class TestAstrokatYAML(unittest.TestCase):
             '--sb-id-code', os.getenv('SB_ID_CODE'),
             '--dry-run',
        ])
-
         # get result and make sure everything ran properly
-        result = self.string_stream.getvalue()
+        result = LoggedTelescope.string_stream.getvalue()
         self.assertIn('Single run through observation target list', result, 'Single run')
-
         self.assertIn(
             ("Imaging targets are ['T3R04C06', 'T4R00C02', 'T4R00C04', 'T4R00C06',"
              " 'T4R01C01', 'T4R01C03', 'T4R01C05', 'T4R02C02', 'T4R02C04']"),
             result, 'Nine imaging targets'
         )
-
-        self.assertIn("Bandpass calibrators are ['1934-638', '3C286']",
-                      result, 'two bandpass calibrators')
-
-        self.assertIn("Gain calibrators are ['1827-360']",
-                      result, 'one gain calibrator')
-
+        self.assertIn(
+            "Bandpass calibrators are ['1934-638', '3C286']", result,
+            'two bandpass calibrators'
+        )
+        self.assertIn("Gain calibrators are ['1827-360']", result, 'one gain calibrator')
         self.assertIn('1827-360 observed for 30.0 sec', result)
         self.assertIn('1934-638 observed for 120.0 sec', result)
         self.assertIn('3C286 observed for 100.0 sec', result)
@@ -157,7 +131,7 @@ class TestAstrokatYAML(unittest.TestCase):
         self.assertIn('T4R02C04 observed for 180.0 sec', result)
 
     def test_image_sim(self):
-        yaml_file = self.yaml_path('test_obs/image-sim.yaml')
+        yaml_file = yaml_path('test_obs/image-sim.yaml')
         observe_main.main([
             '--yaml', yaml_file,
             '--observer', OBSERVER,
@@ -169,7 +143,7 @@ class TestAstrokatYAML(unittest.TestCase):
        ])
 
         # get result and make sure everything ran properly
-        result = self.string_stream.getvalue()
+        result = LoggedTelescope.string_stream.getvalue()
         self.assertIn('Scheduled observation time lapsed - ending observation',
                       result, 'observation time lapsed')
 
