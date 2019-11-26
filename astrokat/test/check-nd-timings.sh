f65d80375fe114250d29208f3c9555e1d8448d37
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2019-11-26 12:47:57 +0200

    add lead_time option to noise diode tests

--- astrokat/test/check-nd-timings.sh
+++ astrokat/test/check-nd-timings.sh
@@ -8,9 +8,13 @@ YELLOW='\033[0;33m'
 NOCOLOR="\033[0m"
 
 INPUT=(
+# set pattern all antennas in the array
 nd-pattern-sim
+# set pattern for only 2 antennas and apply user defined lead time
 nd-pattern-ants
+# deactivate noise diode for target
 nd-pattern-plus-off
+# set trigger time to shorter and longer than lead time
 nd-trigger
 )
 
