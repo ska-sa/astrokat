c8cd4eb38e80fa380038febadc93d975a33c7727
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2020-05-13 18:45:02 +0200

    update test results checks to new log message output

--- astrokat/test/test_nd_timings.py
+++ astrokat/test/test_nd_timings.py
@@ -39,11 +39,7 @@ class TestAstrokatYAML(unittest.TestCase):
             result
         )
         self.assertIn(
-            "Set all noise diodes with timestamp 1573714803",
-            result
-        )
-        self.assertIn(
-            "pattern set at 1573714800.0",
+            "Switch noise-diode pattern on at 1573714803.0",
             result
         )
 
@@ -62,11 +58,11 @@ class TestAstrokatYAML(unittest.TestCase):
 
         result = LoggedTelescope.user_logger_stream.getvalue()
         self.assertIn("No ND for target", result)
-        self.assertIn("Switch noise-diode off at 1573714914.0", result)
+        self.assertIn("noise-diode off at 1573714914.0", result)
         self.assertIn("Restoring ND pattern", result)
         self.assertIn("noise diode pattern every 0.5 sec, with 0.25 sec on",
                       result)
-        self.assertIn("pattern set at 1573715031.0", result)
+        self.assertIn("noise-diode pattern on at 1573715034.0", result)
 
     def test_nd_trigger_long(self):
         """Tests noisediode simulator."""
@@ -75,8 +71,8 @@ class TestAstrokatYAML(unittest.TestCase):
         result = LoggedTelescope.user_logger_stream.getvalue()
         self.assertIn("Firing noise diode for 15.0s", result)
         self.assertIn("Add lead time of 5.0s", result)
-        self.assertIn("Switch noise-diode on at 1573714853.0", result)
-        self.assertIn("Switch noise-diode off at 1573714868.0", result)
+        self.assertIn("noise-diode on at 1573714853.0", result)
+        self.assertIn("noise-diode off at 1573714868.0", result)
 
     def test_nd_trigger_short(self):
         """Tests noisediode simulator."""
@@ -86,7 +82,5 @@ class TestAstrokatYAML(unittest.TestCase):
         self.assertIn("Firing noise diode for 2.0s", result)
         self.assertIn("Add lead time of 5.0s", result)
         self.assertIn("Set noise diode pattern", result)
-        self.assertIn("pattern set at 1573714848.0", result)
-        self.assertIn("Switch noise-diode on at 1573714853.0", result)
-        self.assertIn("Switch noise-diode off at 1573714858.0", result)
+        self.assertIn("noise-diode pattern on at 1573714853.0", result)
         self.assertIn("noise-diode off at 1573714858.0", result)
