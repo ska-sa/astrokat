a4180c958c87335670d78c966bab8dba2d3b3334
Xinyu Wu <xwu@ska.ac.za>
2020-05-19 08:45:34 +1000

    matplotlib<3 can't be installed on ubuntu 20.xx. This won't effect simulation and observation. Just need to make sure users use ubuntu>20 if they want to use plotting utilities of astrokat.

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     zip_safe=False,
     setup_requires=["katversion"],
     use_katversion=True,
-    install_requires=["pyephem", "katpoint", "matplotlib<3", "numpy", "pyyaml"],
+    install_requires=["pyephem", "katpoint", "matplotlib", "numpy", "pyyaml"],
     extras_require={"live": ["katcorelib", "katconf"]},
 )
