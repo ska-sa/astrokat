338e5047c476003c0c550712d11b64c5aff6c3b1
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2020-06-09 19:28:57 +0200

    add missed implementation of 'announce' flag

--- astrokat/simulate.py
+++ astrokat/simulate.py
@@ -243,7 +243,8 @@ class SimSession(object):
         self.track_ = True
         slew_time, az, el = self._fake_slew_(target)
         time.sleep(slew_time)
-        user_logger.info("Slewed to %s at azel (%.1f, %.1f) deg", target.name, az, el)
+        if announce:
+            user_logger.info("Slewed to %s at azel (%.1f, %.1f) deg", target.name, az, el)
         time.sleep(duration)
         if duration > 0:
             user_logger.info("Tracked %s for %d seconds", target.name, duration)
