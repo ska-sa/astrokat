a674dd34f53199b3cd0326c79ff53f45c538a4c7
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2021-04-26 11:44:39 +0200

    update script will now grab/update all mkat catalogues

--- catalogues/update_catalogues.sh
+++ catalogues/update_catalogues.sh
@@ -2,8 +2,18 @@
 
 #Usage: ./update_catalogues.sh ../../katconfig/user/catalogues/
 
+if [ "$#" -lt 1 ]
+then
+    echo "Usage: ./update_catalogues.sh <path to katconfig user catalogues>"
+    echo "  e.g: ./update_catalogues.sh ../../katconfig/user/catalogues/"
+    exit
+fi
+
+# assuming naming convention
+# <L/U>band-<calibrator_classification>-calibrators.csv
+# e.g. L-band calibrators = 'Lband-*-calibrators.csv'
+# e.g. UHF-band calibrators = 'Uband-*-calibrators.csv'
 all_calibrators='*calibrators.csv'
-l_band_calibrators='Lband-*calibrators.csv'
 
 # katconfig catalogues
 katconfig=$1
@@ -14,11 +24,11 @@ today=$(date '+%Y-%m-%d')
 mkdir bak/$today
 
 # update with current catalogues from katconfig
-for file in $l_band_calibrators
+for file in $katconfig/$all_calibrators
 do
     echo $file
     cp $file bak/$today
-    cp $katconfig/$file .
+    cp $file .
 done
 
 # -fin-
