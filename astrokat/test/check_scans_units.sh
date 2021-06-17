9ecb47ee0c479400362920d3b2cdaf88051b22cf
Ruby van Rooyen <ruby.vanrooyen@gmail.com>
2021-06-17 13:10:40 +0200

    remove unit test bash scripts, wrong branch, should be on rvr_obs_devel branch

--- astrokat/test/check_scans_units.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python -m unittest astrokat.test.test_scans.TestAstrokatYAML.test_drift_scan_basic_sim
-python -m unittest astrokat.test.test_scans.TestAstrokatYAML.test_raster_scan_basic_sim
-python -m unittest astrokat.test.test_scans.TestAstrokatYAML.test_scan_basic_sim
