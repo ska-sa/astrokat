d2d91fd9bab942d42a28807b106a899c6a1ef156
Ruby Van Rooyen <ruby@com14.science.kat.ac.za>
2021-08-24 10:56:06 +0000

    remove redundant test scripts

--- astrokat/test/check_offline_observe_units.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_targets_sim
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_two_calib_sim
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_image_single_sim
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_image_sim
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_solar_body
-python -m unittest astrokat.test.test_offline_observe.TestAstrokatYAML.test_below_horizon
