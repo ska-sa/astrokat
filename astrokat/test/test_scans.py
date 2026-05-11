9655474d5859988e401d71e18f323974502d738d
nmakhoba <nmakhoba@sarao.ac.za>
2026-05-11 16:44:02 +0200

    Use passed session_name env to determine target values to be used for the observation

--- astrokat/test/test_scans.py
+++ astrokat/test/test_scans.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import os
 import unittest
 
 import katpoint
@@ -32,7 +33,12 @@ class TestAstrokatYAML(unittest.TestCase):
         result = LoggedTelescope.user_logger_stream.getvalue()
         self.assertIn("Initialising Drift_scan target 1934-638 for 180.0 sec", result)
         self.assertIn("Drift_scan observation for 180.0 sec", result)
-        target_string = "Az: -159:01:46.8 El: 52:05:00.6"
+        # use running session to determine suitable target values
+
+        if os.getenv("SESSION_NAME") == "mkat_session":
+            target_string = "Az: -159:01:46.8 El: 52:05:00.6"
+        else:
+            target_string = "Az: -158:55:32.5 El: 52:01:18.0"
         self.assert_started_target_track(target_string, 180.0, result)
         self.assert_completed_target_track(target_string, 180.0, result)
 
